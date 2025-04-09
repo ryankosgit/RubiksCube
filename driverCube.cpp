9455516c5b7bc7631c3311287bc3c1278a36dfec
ryankosgit <rb22br@fsu.edu>
2025-04-08 23:15:53 -0400

    Update driverCube.cpp

--- driverCube.cpp
+++ driverCube.cpp
@@ -2,14 +2,10 @@
 #include "RubiksCube.h"
 using namespace std;
 
-int main()
-{
-    
-    RubiksCube a1; // Constructor initializes the cube
-    a1.setCube();
-    a1.displayFaces();
-    cout << endl;
-    a1.userInput();
 
-    return 0;
-}
+int main(){
+    RubiksCube a1;
+    a1.display();
+    a1.userinput();
+
+};
