cd297ce56ff0e201206cb21a286b99888b97146f
ryankosgit <rb22br@fsu.edu>
2025-04-08 23:14:48 -0400

    Update RubiksCube.h

--- RubiksCube.h
+++ RubiksCube.h
@@ -1,45 +1,30 @@
-//rubikscube.h
-#include <iostream>
+//rubiks cube .h
+#include <string>
 using namespace std;
 
-class RubiksCube
-{
+class RubiksCube{
 public:
+    RubiksCube();
+    void counterclockwise(int x);
+    void clockwise(int x);
+    void display() const;
+    void userinput();
+    void executeScramble(string x);
+    void rotateLayer(int face, bool clockwise);
+
+private:
+    string cube[3][3][6];
+    bool userinputbool;
+    string userInputStr;
+    bool generatedScramble;
+    string scrambleinput;
+    void rotateFace(int x);
+    void rotateFacePrime(int x);
     const string red = "\033[41m  \033[0m";
     const string green = "\033[42m  \033[0m";
     const string blue = "\033[44m  \033[0m";
     const string yellow = "\033[48;2;255;255;0m  \033[0m"; 
     const string white = "\033[48;2;255;255;255m  \033[0m"; 
     const string orange = "\033[48;2;255;165;0m  \033[0m"; 
-
-
-    RubiksCube(); // Initialize the cube
-    void displayFaces();
-    void userInput();
-    void executeScramble();
-
-    void turnRight();
-    void turnRightP();
-    void turnLeft();
-    void turnLeftP();
-    void turnUp();
-    void turnUpP();
-    void turnDown();
-    void turnDownP();
-    void turnBack();
-    void turnBackP();
-    void turnFront();
-    void turnFrontP();
-
-private:
-    bool userTrue;
-    bool generatedScramble;
-    string userInputStr;
-    string frontface[3][3];
-    string upface[3][3];
-    string rightface[3][3];
-    string leftface[3][3];
-    string downface[3][3];
-    string backface[3][3];
-    string scrambleinput;
+    string colors[6] = {white, yellow, red, orange, blue, green};
 };
